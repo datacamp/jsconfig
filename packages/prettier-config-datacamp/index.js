31a12d9d1f6d2d842079af5cc29f9a0e7555ee23
Jens Claes <jens@datacamp.com>
2020-04-10 15:50:58 +0200

    fix(prettier): remove default settings

--- packages/prettier-config-datacamp/index.js
+++ packages/prettier-config-datacamp/index.js
@@ -1,18 +1,10 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSpacing: true,
-  jsxBracketSameLine: false,
   overrides: [
     {
       files: ['*.json'],
       options: { parser: 'json' },
     },
   ],
-  printWidth: 80,
-  proseWrap: 'preserve',
-  semi: true,
   singleQuote: true,
-  tabWidth: 2,
   trailingComma: 'all',
-  useTabs: false,
 };
