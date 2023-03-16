c8e10232af2409109ee8804b130c268e8da203c2
Szymon Bury <szymon.bury@datacamp.com>
2023-03-16 08:58:58 +0100

    feat: e2e-spec files as test files

--- packages/eslint-config-datacamp/helpers.js
+++ packages/eslint-config-datacamp/helpers.js
@@ -1,5 +1,5 @@
 exports.testFilesGlobPatterns = [
-  '**/*.{spec,test}.{js,jsx,ts,tsx}',
+  '**/*.{spec,e2e-spec,test,e2e-test}.{js,jsx,ts,tsx}',
   '**/__mocks__/**',
   '**/{test,tests,spec,specs,__mocks__}/**',
   '**/{cypress}/**',
